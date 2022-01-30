7b7c6b5f3a84f5edc765e9ef2c8a9ec81e3f16b8
leandrotn <74495390+leandrotn@users.noreply.github.com>
2022-01-30 02:23:10 -0300

    Update onliner.js

--- onliner.js
+++ onliner.js
@@ -1,6 +1,15 @@
 var fs = require('fs')
 const Discord = require("discord.js11");
 var chalk = require('chalk');
+const express = require("express");
+const app = express();
+app.get("/", (request, response) => {
+  const ping = new Date();
+  ping.setHours(ping.getHours() - 3);
+  console.log(`Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`);
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT); // Recebe solicitações que o deixa online
 
 const tokens = fs.readFileSync('tokens.txt', 'utf-8').replace(/\r|\x22/gi, '').split('\n');
 
@@ -20,7 +29,7 @@ class Bot {
     online() {
         this.bot = new Discord.Client();
         this.bot.on('ready', () => {
-            console.log(chalk.green(`[SISTEMA]`) + `|` + (chalk.blueBright `Conectado! `) + (chalk.redBright `%s`) + ` |` + (chalk.blueBright ` ID: `) + (chalk.magenta `%s`) + ` at` + (chalk.yellow ` "%s"`), this.bot.user.tag, this.bot.user.id, new Date().toLocaleTimeString());
+            console.log(chalk.green(`[SISTEMA]`) + `|` + (chalk.blueBright `Conectado Em: `) + (chalk.redBright `%s`) + ` |` + (chalk.blueBright ` ID: `) + (chalk.magenta `%s`) + ` Horário:` + (chalk.yellow ` "%s"`), this.bot.user.tag, this.bot.user.id, new Date().toLocaleTimeString());
             this.bot.on("message", async message => {})
         });
         this.bot.on('ready', () => {
