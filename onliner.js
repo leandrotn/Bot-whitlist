2b4cde7f873c9091bccd4b96e0645fabfe360c9b
leandrotn <74495390+leandrotn@users.noreply.github.com>
2022-01-30 01:57:25 -0300

    Update onliner.js

--- onliner.js
+++ onliner.js
@@ -4,7 +4,6 @@ var chalk = require('chalk');
 
 const tokens = fs.readFileSync('tokens.txt', 'utf-8').replace(/\r|\x22/gi, '').split('\n');
 
-var items = ['Minecraft', 'Rust', 'VRChat', 'reeeee', 'MORDHAU', 'Fortnite', 'Apex Legends', 'Escape from Tarkov', 'Rainbow Six Siege', 'Counter-Strike: Global Offense', 'Sinner: Sacrifice for Redemption', 'Minion Masters', 'King of the Hat', 'Bad North', 'Moonlighter', 'Frostpunk', 'Starbound', 'Masters of Anima', 'Celeste', 'Dead Cells', 'CrossCode', 'Omensight', 'Into the Breach', 'Battle Chasers: Nightwar', 'Red Faction Guerrilla Re-Mars-tered Edition', 'Spellforce 3', 'This is the Police 2', 'Hollow Knight', 'Subnautica', 'The Banner Saga 3', 'Pillars of Eternity II: Deadfire', 'This War of Mine', 'Last Day of June', 'Ticket to Ride', 'RollerCoaster Tycoon 2: Triple Thrill Pack', '140', 'Shadow Tactics: Blades of the Shogun', 'Pony Island', 'Lost Horizon', 'Metro: Last Light Redux', 'Unleash', 'Guacamelee! Super Turbo Championship Edition', 'Brutal Legend', 'Psychonauts', 'The End Is Nigh', 'Seasons After Fall', 'SOMA', 'Trine 2: Complete Story', 'Trine 3: The Artifacts of Power', 'Trine Enchanted Edition', 'Slime-San', 'The Inner World', 'Bridge Constructor', 'Bridge Constructor Medieval', 'Dead Age', 'Risk of Rain', "Wasteland 2: Director's Cut", 'The Metronomicon: Slay The Dance Floor', 'TowerFall Ascension + Expansion', 'Nidhogg', 'System Shock: Enhanced Edition', 'System Shock 2', "Oddworld:New 'n' Tasty!", 'Out of the Park Baseball 18', 'Hob', 'Destiny 2', 'Torchlight', 'Torchlight 2', 'INSIDE', 'LIMBO', "Monaco: What's Yours Is Mine", 'Tooth and Tail', 'Dandara', 'GoNNER', 'Kathy Rain', 'Kingdom: Classic', 'Kingdom: New Lands', 'Tormentor X Punisher', 'Chaos Reborn', 'Ashes of the Singularity: Escalation', 'Galactic Civilizations III', 'Super Meat Boy', 'Super Hexagon', 'de Blob 2', 'Darksiders II Deathinitive Edition', 'Darksiders Warmastered Edition', 'de Blob', 'Red Faction 1', 'Dungeon Defenders'];
 
 process.on('uncaughtException', e => {});
 process.on('uncaughtRejection', e => {});
@@ -27,7 +26,7 @@ class Bot {
         this.bot.on('ready', () => {
             this.bot.user.setPresence({
                 game: {
-                    name: items[Math.floor(Math.random() * items.length)], //A TA BETÁ
+                    name: items[Math.floor(Math.random() * items.length)], //A tá beta 
                     type: "PLAYING",
                     url: "http://leandrotn.ga"
                 }
@@ -38,15 +37,15 @@ class Bot {
     }
 }
 
-process.title = `[SERVER] Contas Online: ${tokens.length}`;
-console.log(chalk.green(`Contas Online!`)) + (chalk.cyanBright(`[SISTEMA] `)) + (chalk.red(`Contas Online: ${tokens.length}`)));
+process.title = ` [SISTEMA] Tokens Online: ${tokens.length}`;
+console.log(chalk.green(`DEIXANDO OS TOKEN Onliner: `) + (chalk.magenta(`Tá on! `)) + (chalk.cyanBright(`[SISTEMA] `)) + (chalk.red(`Tokens Online: ${tokens.length}`)));
 console.log ("")
 console.log(" ▒██░░░░▐█▀▀░░▄█▀▄─▒██▄░▒█▌░▐█▀█▄▒▐█▀▀▄▒▐█▀▀█▌▒█▀█▀█▒██▄░▒█▌")
 console.log(" ▒██░░░░▐█▀▀░▐█▄▄▐█▒▐█▒█▒█░░▐█▌▐█▒▐█▒▐█▒▐█▄▒█▌░░▒█░░▒▐█▒█▒█░")
 console.log(" ▒██▄▄█░▐█▄▄░▐█─░▐█▒██░▒██▌░▐█▄█▀▒▐█▀▄▄▒▐██▄█▌░▒▄█▄░▒██░▒██▌")
-console.log ("Kiba Ae Vacilão , é vai aprender A programar!")
+console.log ("")
 console.log(chalk.underline(chalk.green(`                        By:LEANDROTN#0101`)));
-console.log(chalk.inverse(`[SISTEMA] - [Token Onliner]`));
+console.log(chalk.inverse(`[SISTEMA] - [Tokens Online!]`));
 
 console.log();
 console.log();
